3d9943abb3ca622a293ec192f97d150da86be6b4
TheKasaiRyuu <league@Mac-mini-2.local>
2016-05-04 15:00:38 -0700

    Break;
    break; stops things

---
+++ src/Ages.java
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane;
+
+public class Ages {
+public static void main(String[] args) {
+	int year = JOptionPane.show
+	for (int i = 0; i < 16; i++) {
+		JOptionPane.showMessageDialog(null, "In " + year + " I was " + i);
+		year = year + 1;
+	}
+	
+	
+	
+	
+	
+}
+}
